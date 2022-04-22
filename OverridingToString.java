457a0f6b902c7bb07753cb7ed21ef1022e1e7236
Lrmi <104178738+Lrmi@users.noreply.github.com>
2022-04-22 11:44:27 +0900

    Update OverridingToString.java

--- OverridingToString.java
+++ OverridingToString.java
@@ -1,15 +1,4 @@
 package chapter16;
-class Cake{
-	public String toString() {
-		System.out.println(super.toString());
-		return"My birthday cake";
-	}
-}
-class CheeseCake extends Cake{
-	public String toString() {
-		return "My birthday cheese cake";
-	}
-}
 public class OverridingToString {
 	public static void main(String[] args) {
 		Cake c1 =new Cake();
